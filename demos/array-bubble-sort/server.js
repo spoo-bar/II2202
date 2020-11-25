9b83690e68eb00935b40092b948a2e79106dcf53
Spoorthi S <spoon611@users.noreply.github.com>
2020-11-25 20:12:17 +0100

    supporting session creation in server

--- demos/array-bubble-sort/server.js
+++ demos/array-bubble-sort/server.js
@@ -4,11 +4,38 @@ var app = express();
 var http = require('http').Server(app);
 var JIFFServer = require('../../lib/jiff-server');
 new JIFFServer(http, { logs:true });
+const { v4: uuidv4 } = require('uuid');
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+
+var sessions = [];
+
+// API routes
+app.get('/session', function (req, res) {
+  res.send(sessions);
+});
+
+app.post('/session', function (req, res) {
+  var description = req.body.description;
+  var participants = req.body.participants;
+  createSession(description, participants);
+  res.send(true);
+});
 
 // Serve static files.
 app.use('/demos', express.static(path.join(__dirname, '..', '..', 'demos')));
 app.use('/dist', express.static(path.join(__dirname, '..', '..', 'dist')));
 app.use('/lib/ext', express.static(path.join(__dirname, '..', '..', 'lib', 'ext')));
+
+// Seed data
+createSession('Stockholm', 5);
+createSession('Hanover', 3);
+createSession('Bangalore', 6);
+
+
 http.listen(8080, function () {
   console.log('listening on *:8080');
 });
@@ -16,3 +43,12 @@ http.listen(8080, function () {
 console.log('Direct your browser to http://localhost:8080/demos/array-bubble-sort/client.html.');
 console.log('To run a node.js based party: node demos/array-bubble-sort/party <input>');
 console.log();
+
+function createSession(description, participants) {
+  var session = {
+    id: uuidv4(),
+    description: description,
+    participants: participants
+  };
+  sessions.push(session);
+}
