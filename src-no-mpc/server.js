b585bb4b60ec989edb0ce907caf439512f1b1ae7
Spoorthi S <spoon611@users.noreply.github.com>
2021-01-10 14:13:13 +0100

    server changes for npn mpc implementation

--- src-no-mpc/server.js
+++ src-no-mpc/server.js
@@ -12,16 +12,28 @@ wsServer = new WebSocketServer({
   httpServer: server
 });
 
+var connections = [];
+
 // WebSocket server
 wsServer.on('request', function(request) {
-  var connection = request.accept(null, request.origin);
+  var connection = request.accept('non-mpc-benchmark', request.origin);
 
   // This is the most important callback for us, we'll handle
   // all messages from users here.
   connection.on('message', function(message) {
     if (message.type === 'utf8') {
-      willcompute.message.
       // process WebSocket message
+      let data = JSON.parse(message);
+      if(connections.length == data.party_count) {
+        sendSortedData();
+      }
+      else {        
+        let client = {
+          data: data,
+          connection: connection
+        };
+        connections.push(client);
+      }
     }
   });
 
@@ -29,3 +41,13 @@ wsServer.on('request', function(request) {
     // close user connection
   });
 });
+
+function sendSortedData() {
+  let data_array = connections.map(c => c.data);
+  let inputs_array = data_array.map(d => d.input);
+  // todo sort
+  connections.forEach(connection => {
+    let jsonData = JSON.stringify(inputs_array);
+    connection.sendUTF(jsonData);
+  });
+}
