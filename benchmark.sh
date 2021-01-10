9256125e54c459390a8f4e24136a3dd5581e77bc
Jasper Bernhardt <jasper.bernhardt@live.de>
2021-01-10 15:14:32 +0100

    Add benchmark for for non-mpc version NOT VERIFIED. Need to transfer to VM

--- benchmark.sh
+++ benchmark.sh
@@ -6,10 +6,11 @@ DELAYS_MS=(0 5 10 15 20)
 TIMEFORMAT=%R
 DOCKER_TAG=mpcbenchmark
 DOCKER_CONTAINER_NAME="$DOCKER_TAG"
+DOCKER_CLIENT_PARTY_CMD_PATH=src/array-bubble-sort/party.js
 
 function startOneClient {
     RANDOM_NUMBER=$(( ( RANDOM % 1000 )  + 1 ))
-    docker exec "$DOCKER_CONTAINER_NAME" node src/array-bubble-sort/party.js "[$RANDOM_NUMBER]" "$1" "$2" > /dev/null;
+    docker exec "$DOCKER_CONTAINER_NAME" node "$DOCKER_CLIENT_PARTY_CMD_PATH" "[$RANDOM_NUMBER]" "$1" "$2" > /dev/null;
 }
 
 function startNumberOfClients {
@@ -52,10 +53,23 @@ echo "Starting benchmark. You should not have the server running and be in the r
 echo "Building docker container.."
 docker build -q -t "$DOCKER_TAG" . > /dev/null
 
-echo "Starting Node in node. Will be killed on exit"
+echo "Starting Node in docker with mpc. Will be killed on exit"
+docker run --cap-add=NET_ADMIN -d --rm --name="$DOCKER_CONTAINER_NAME" "$DOCKER_TAG" "node" "src-no-mpc/server.js" > /dev/null
+trap "docker rm -f $DOCKER_CONTAINER_NAME > /dev/null; exit;" EXIT SIGINT
+echo "Waiting 15 sec for container to start"
+sleep 15
+
+for delay in "${DELAYS_MS[@]}"; do
+    runWithDelay "$delay"
+done
+
+
+
+docker rm -f $DOCKER_CONTAINER_NAME > /dev/null
+echo "Starting Node in docker without mpc. Will be killed on exit"
 docker run --cap-add=NET_ADMIN -d --rm --name="$DOCKER_CONTAINER_NAME" "$DOCKER_TAG" > /dev/null
 trap "docker rm -f $DOCKER_CONTAINER_NAME > /dev/null; exit;" EXIT SIGINT
-
+DOCKER_CLIENT_PARTY_CMD_PATH="src-no-mpc/client.js"
 echo "Waiting 15 sec for container to start"
 sleep 15
 
